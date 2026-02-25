7727d30c1ed937773657c3bed8cc9782fe9feea2
allyzahs <work.allyzahsantillan@gmail.com>
2026-02-25 20:11:46 +0800

    Add tests for Inventory Page

--- pages/InventoryPage.js
+++ pages/InventoryPage.js
@@ -1,8 +1,9 @@
 import { expect } from "@playwright/test";
+import { CartHeader } from "../components/CartHeader";
 
-export class InventoryPage {
+export class InventoryPage extends CartHeader{
   constructor(page) {
-    this.page = page;
+    super(page);
     this.inventoryContainer = this.page.locator('.inventory_container');
     this.productCard = this.page.locator('.inventory_item');
     this.productTitle = this.productCard.locator('.inventory_item_name');
@@ -12,12 +13,6 @@ export class InventoryPage {
     this.addButton = (productKey) => this.page.locator(`[data-test="add-to-cart-${productKey}"]`);
     this.removeButton = (productKey) => this.page.locator(`[data-test="remove-${productKey}"]`);
     this.sortDropdown = this.page.locator('select.product_sort_container');
-    this.sortOptions = {
-      az: 'az',         // Name (A to Z)
-      za: 'za',         // Name (Z to A)
-      lohi: 'lohi',     // Price (low to high)
-      hilo: 'hilo',     // Price (high to low)
-    }
   }
 
   async open() {
@@ -42,7 +37,13 @@ export class InventoryPage {
       })
     }
 
-    return displayedProducts;
+    const cleanedDisplayedProducts = displayedProducts.map(p => ({
+      ...p,
+      price: p.price.replace('$', '')
+    }));
+
+
+    return cleanedDisplayedProducts;
   }
 
   async validateDisplayedProducts(expectedProducts) {
@@ -50,14 +51,85 @@ export class InventoryPage {
     const sorted = arr =>
       arr.slice().sort((a, b) => a.title.localeCompare(b.title));
     
-    const cleanedDisplayedProducts = displayedProducts.map(p => ({
-      ...p,
-      price: p.price.replace('$', '')
-    }));
+    expect(sorted(displayedProducts)).toEqual(sorted(expectedProducts));
+  }
 
-    expect(sorted(cleanedDisplayedProducts)).toEqual(sorted(expectedProducts));
-  } 
+  async addToCart(productName) {
+    let productKey = productName.toLowerCase().replaceAll(' ', '-');
+    await this.addButton(productKey).click();
+  }
 
-  
+  async removeFromCart(productName) {
+    let productKey = productName.toLowerCase().replaceAll(' ', '-');
+    await this.removeButton(productKey).click();
+  }
+
+  async addMultipleItemsToCart(products, numberOfItemsToAdd) {
+    let productsAdded = [];
+     for(let i = 0; i < numberOfItemsToAdd; i++) {
+        await this.addToCart(products[i].title);
+        productsAdded.push(products[i]);
+    }
+
+    return productsAdded;
+  }
+
+  async removeMultipleItemsFromCart(products, numberOfItemsToRemove) {
+    let productsRemoved = [];
+     for(let i = 0; i < numberOfItemsToRemove; i++) {
+        await this.removeFromCart(products[i].title);
+        productsRemoved.push(products[i]);
+    }
+
+    return productsRemoved;
+  }
+
+  async openProductByTitle(productIndex) {
+    await this.productTitle.nth(productIndex).click();
+  }
+
+  async openProductByImage(productIndex) {
+    await this.productImage.nth(productIndex).click();
+  }
+
+  async validateThatProductItemIsDisplayed() {
+    await expect(this.page).toHaveURL(/inventory-item/);
+  }
+
+  async sortDisplayedProducts(option) {
+    await this.sortDropdown.selectOption({ label: option });
+  }
+
+  async validateThatProductsAreSortedCorrectly(option, expectedProductLists) {
+    const displayedSortedProducts = await this.getDisplayedProductDetails();
+    const sortedByTitle = arr =>
+      arr.slice().sort((a, b) => a.title.localeCompare(b.title));
+
+    const sortByPriceThenTitle = (products, order = 'asc') =>
+      products.slice().sort((a, b) => {
+        if (a.price !== b.price) {
+          return order === 'asc'
+            ? a.price - b.price
+            : b.price - a.price;
+        }
+
+      return a.title.localeCompare(b.title);
+    });
+
+    switch(option) {
+      case 'Name (A to Z)':
+        expect(sortedByTitle(expectedProductLists)).toEqual(displayedSortedProducts);
+        break;
+      case 'Name (Z to A)':
+        expect(sortedByTitle(expectedProductLists).reverse()).toEqual(displayedSortedProducts);
+        break;
+      case 'Price (low to high)':
+        expect(sortByPriceThenTitle(expectedProductLists)).toEqual(displayedSortedProducts);
+        break;
+      case 'Price (high to low)':
+        expect(sortByPriceThenTitle(expectedProductLists, 'desc')).toEqual(displayedSortedProducts);
+        break;
+    }
+  }
 
 }
--- tests/inventory.spec.js
+++ tests/inventory.spec.js
@@ -9,38 +9,68 @@ test('Validate that each product has the title, description, and the price displ
     await inventoryPage.validateDisplayedProducts(productList);
 });
 
-test('Validate that one product is added to the cart', async() => {
+test('Validate that cart counter is incremented by 1 after adding 1 item', async({inventoryPage}) => {
+    const displayedProducts = await inventoryPage.getDisplayedProductDetails();
+    const productIndex = Math.floor(Math.random() * displayedProducts.length);
+    const beforeAddToCart = await inventoryPage.getNumberOfItemsInCart();
 
+    await inventoryPage.addToCart(displayedProducts[productIndex].title);
+    await inventoryPage.validateNumberOfItemsInCart(beforeAddToCart + 1);
 });
 
-test('Validate that multiple products are added to the cart', async()  => {
-
+test('Validate that cart counter is incremented based on the number of multiple items added', async({inventoryPage})  => {
+    const displayedProducts = await inventoryPage.getDisplayedProductDetails();
+    const numberOfItemsToAdd = (Math.floor(Math.random() * displayedProducts.length)) + 1;
+    //Make sure that cart is empty
+    await inventoryPage.validateNumberOfItemsInCart(0);
+    await inventoryPage.addMultipleItemsToCart(displayedProducts, numberOfItemsToAdd);
+    await inventoryPage.validateNumberOfItemsInCart(numberOfItemsToAdd);
 });
 
-test('Validate that one product is removed from the cart', async() => {
+test('Validate that cart counter is decremented by 1 after removing 1 item', async({inventoryPage}) => {
+    const displayedProducts = await inventoryPage.getDisplayedProductDetails();
+    const numberOfItemsToAdd = (Math.floor(Math.random() * displayedProducts.length)) + 1;
+    //Make sure that cart is empty
+    await inventoryPage.validateNumberOfItemsInCart(0);
+    //Make sure that there will items to be removed
+    const productsAdded = await inventoryPage.addMultipleItemsToCart(displayedProducts, numberOfItemsToAdd);
 
+    const productIndex = Math.floor(Math.random() * productsAdded.length);
+    await inventoryPage.removeFromCart(productsAdded[productIndex].title);
+    await inventoryPage.validateNumberOfItemsInCart(numberOfItemsToAdd - 1);
 });
 
-test('Validate that multiple products can be removed from the cart', async() => {
+test('Validate that cart counter is decremented based on the number of multiple items removed', async({inventoryPage}) => {
+    const displayedProducts = await inventoryPage.getDisplayedProductDetails();
+    const numberOfItemsToAdd = (Math.floor(Math.random() * displayedProducts.length)) + 1;
+    //Make sure that cart is empty
+    await inventoryPage.validateNumberOfItemsInCart(0);
+    //Make sure that there will items to be removed
+    const productsAdded = await inventoryPage.addMultipleItemsToCart(displayedProducts, numberOfItemsToAdd);
 
+    const numberOfItemsToRemove = (Math.floor(Math.random() * productsAdded.length)) + 1;
+    await inventoryPage.removeMultipleItemsFromCart(productsAdded, numberOfItemsToRemove);
+    await inventoryPage.validateNumberOfItemsInCart(numberOfItemsToAdd - numberOfItemsToRemove);
 });
 
-test('Validate that products details can be viewed or opened in Product View Page', async() => {
-
+test('Validate that products details can be viewed or opened in Product View Page by clicking Product Title', async({inventoryPage}) => {
+    const displayedProducts = await inventoryPage.getDisplayedProductDetails();
+    const productIndex = Math.floor(Math.random() * displayedProducts.length);
+    await inventoryPage.openProductByTitle(productIndex);
+    await inventoryPage.validateThatProductItemIsDisplayed();
 });
 
-test('Validate that products are sorted by Name in Ascending order', async() => {
-
+test('Validate that products details can be viewed or opened in Product View Page by clicking Product Image', async({inventoryPage}) => {
+    const displayedProducts = await inventoryPage.getDisplayedProductDetails();
+    const productIndex = Math.floor(Math.random() * displayedProducts.length);
+    await inventoryPage.openProductByImage(productIndex);
+    await inventoryPage.validateThatProductItemIsDisplayed();
 });
 
-test('Validate that products are sorted by Name in Descending order', async() => {
-
-});
-
-test('Validate that products are sorted by Price in Ascending order', async() => {
-
-});
-
-test('Validate that products are sorted by Price in Descending order', async() => {
-
+const sortingOptions = ['Name (A to Z)', 'Name (Z to A)', 'Price (low to high)', 'Price (high to low)'];
+sortingOptions.forEach( option => {
+    test(`Validate that products are sorted by ${option}`, async({inventoryPage}) => {
+        await inventoryPage.sortDisplayedProducts(option);
+        await inventoryPage.validateThatProductsAreSortedCorrectly(option, productList);
+    });
 });
