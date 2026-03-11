46c32245dce72f0a922ac4110962ffa645737daa
allyzahs <work.allyzahsantillan@gmail.com>
2026-03-11 17:13:59 +0800

    Add invalid access tests

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,30 +1,34 @@
-import { expect } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import { BasePage } from "./basePage";
 import { SideBarMenu } from "../components/SideBarMenu";
 
 export class LoginPage extends BasePage {
   constructor(page) {
     super(page);
-    this.username = this.page.locator('#user-name');
-    this.password = this.page.locator('#password');
+    this.username = this.page.getByPlaceholder('Username');
+    this.password = this.page.getByPlaceholder('Password');
     this.loginBtn = this.page.locator('#login-button');
     this.errorMsgContainer = this.page.locator('.error-message-container');
     
     this.sidebar = new SideBarMenu(page);
   }
 
+  // Not allowed to have test.step()
   async fillUsername(username) {
-    await this.username.fill(username);
+      await this.username.fill(username);
   }
 
+  // Not allowed to have test.step()
   async fillPassword(password) {
     await this.password.fill(password);
   }
 
+  // Not allowed to have test.step()
   async clickLoginBtn() {
     await this.loginBtn.click();
   }
 
+  // Not allowed to have test.step()
   async login(username, password) {
     await this.fillUsername(username);
     await this.fillPassword(password);
@@ -32,21 +36,34 @@ export class LoginPage extends BasePage {
   }
 
   async validateLoginErrorMsg(message) {
-    await expect(this.errorMsgContainer).toContainText(message);
+    test.step(`Validate error message - "${message}" is displayed`, async() => {
+      await expect(this.errorMsgContainer).toContainText(message);
+    });
   }
 
   async validateUserIsLoggedIn() {
-    await expect(this.inventoryContainer).toBeVisible();
+    test.step('Validate that Inventory Page is visible', async() => {
+      await expect(this.inventoryContainer).toBeVisible();;
+    });
   }
 
   async validateLoginScreen() {
-    await expect(this.username).toBeVisible();
-    await expect(this.password).toBeVisible();
-    await expect(this.loginBtn).toBeVisible();
+    test.step('Validate that Login Elements - Username, Password, Login Button are visible', async() => {
+      await expect(this.username).toBeVisible();
+      await expect(this.password).toBeVisible();
+      await expect(this.loginBtn).toBeVisible();
+    }); 
   }
 
   async validateLoginFieldsToBeEmpty() {
-    await expect(this.username).toContainText('');
-    await expect(this.password).toContainText('');
+    test.step('Validate that Username and Password Fields are blank', async() => {
+      await expect(this.username).toContainText('');
+      await expect(this.password).toContainText('');
+    }); 
+  }
+
+  async validateInvalidAccess(errorMsg, url) {
+    const message = errorMsg.replace('url', url);
+    await this.validateLoginErrorMsg(message);
   }
 }
